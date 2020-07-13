ac7cf08fa92c46d87ed8707a5901c325efff8aed
hannahjorich <hannah.jorich@gmail.com>
2020-07-12 18:18:24 -0600

    added what if statements/else

--- script.js
+++ script.js
@@ -51,4 +51,71 @@ if(passwordlength < 8 || passwordlength > 128){
 
       choices = character.concat(number, alpha, alpha2);
   }
+
+  // Else if 3 are true 
+  else if (confirmCharacter && confirmNumber && confirmUppercase) {
+    choices = character.concat(number, alpha2);
 }
+else if (confirmCharacter && confirmNumber && confirmLowercase) {
+    choices = character.concat(number, alpha);
+}
+else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+    choices = character.concat(alpha, alpha2);
+}
+else if (confirmNumber && confirmLowercase && confirmUppercase) {
+    choices = number.concat(alpha, alpha2);
+}
+// Else if for 2 are true
+else if (confirmCharacter && confirmNumber) {
+    choices = character.concat(number);
+
+} else if (confirmCharacter && confirmLowercase) {
+    choices = character.concat(alpha);
+
+} else if (confirmCharacter && confirmUppercase) {
+    choices = character.concat(alpha2);
+}
+else if (confirmLowercase && confirmNumber) {
+    choices = alpha.concat(number);
+
+} else if (confirmLowercase && confirmUppercase) {
+    choices = alpha.concat(alpha2);
+
+} else if (confirmNumber && confirmUppercase) {
+    choices = number.concat(alpha2);
+}
+// Else if for 1 is true 
+else if (confirmCharacter) {
+    choices = character;
+}
+else if (confirmNumber) {
+    choices = number;
+}
+else if (confirmLowercase) {
+    choices = alpha;
+}
+// Created space variable to fill uppercase conversion
+else if (confirmUppercase) {
+    choices = space.concat(alpha2);
+};
+
+// password variable is an array placeholder for user generated amount of length
+var password = [];
+
+// Start random selection variables:
+// Random selection for all variables: 
+for (var i = 0; i < enter; i++) {
+    var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+    password.push(pickChoices);
+}
+
+var ps = password.join("");
+UserInput(ps);
+return ps;
+}
+// This puts the password value into the textbox - but when I try it's blank 
+function UserInput(ps) {
+document.getElementById("password").textContent = ps;
+
+}
+
